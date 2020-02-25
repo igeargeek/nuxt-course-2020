f76aaf7784864b12c3de7c1392058c6c8d1e160c
Arthorn kittinukul <joker.arthorn@gmail.com>
2020-02-25 16:19:15 +0700

    [Added] folder myprofile, myprofile answer, form input

---
+++ My-Profile-App/myProfile-Answer/components/formInput.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <b-row>
+      <b-col>
+        <b-form-input class="mt-4" v-model="fullName" placeholder="Enter your name"></b-form-input>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <b-form-input class="mt-4" v-model="email" placeholder="Enter your email"></b-form-input>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <b-form-textarea class="mt-4" v-model="description" placeholder="Enter description"/>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <b-button class="mt-4" variant="outline-primary" @click="onSubmit">Submit</b-button>
+        <b-button class="mt-4" variant="outline-warning" @click="onClear">Clear</b-button>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+<script>
+export default {
+  name: 'formInput',
+  data() {
+    return {
+      fullName: '',
+      email: '',
+      description: ''
+    }
+  },
+  methods: {
+    onClear() {
+      this.fullName = ''
+      this.email = ''
+      this.description = ''
+    },
+    onSubmit() {
+      this.$emit('submitForm', {
+        fullname: this.fullName,
+        email: this.email,
+        description: this.description
+      }) 
+    }
+  }
+}
+</script>
---
+++ My-Profile-App/myProfile-Answer/pages/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <b-container>
+    <h1>My profile</h1>
+    <formInput @submitForm="submitForm"/>
+  </b-container>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import formInput from '~/components/formInput'
+
+export default {
+  components: {
+    Logo,
+    formInput
+  },
+  methods: {
+    submitForm(value) {
+      console.log('value: ', value);
+    }
+  }
+
+}
+</script>
+
+<style>
+/* .container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+} */
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
